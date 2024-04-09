9fabd25aff163c039b436bb05bd79a0832f99174
yyxxmm-codepar <yyxxmm@codepartners.co.kr>
2024-04-09 20:41:43 +0900

    윤성미: 전자문서경영평가 완료

--- js/template.js
+++ js/template.js
@@ -178,7 +178,7 @@ $(document).ready(function(){
     acrdBtnImg = $('.accordion img, .accordion_body img');
     
   acrdCont.hide();
-  acrdCont.first().show();
+  // acrdCont.first().show(); /* 'default : 닫혀있음'으로 변경, cp-20240400 */
   $('.accordion_body .cnt').hide();
 
   acrdItem.click(function(){
@@ -209,33 +209,33 @@ $(document).ready(function(){
 
 
   /* table context menu *//* cp-20240400 */
-  $(document).ready(function(){
-    var contextMenus = document.querySelectorAll(".contextmenu");
-    contextMenus.forEach(function(menu, index) {
-      var table = menu.previousElementSibling;
-      function showMenu(e) {
+$(document).ready(function(){
+  var contextMenus = document.querySelectorAll(".contextmenu");
+  contextMenus.forEach(function(menu,index) {
+    var table = menu.previousElementSibling;
+    function showMenu(e) {
+      e.preventDefault();
+      contextMenus.forEach(function(otherMenu, otherIndex) {
+        otherMenu.style.display=otherIndex===index?"block":"none";
+      });
+      menu.style.top=e.pageY+"px";
+      menu.style.left=e.pageX+"px";
+    }
+    table.addEventListener("contextmenu", showMenu);
+    table.addEventListener("click", function() {
+      menu.style.display="none";
+    });
+    menu.querySelectorAll("li a").forEach(function(item) {
+      item.addEventListener("click", function(e) {
         e.preventDefault();
-        contextMenus.forEach(function(otherMenu, otherIndex) {
-          otherMenu.style.display = otherIndex === index ? "block" : "none";
-        });
-        menu.style.left = e.pageX + "px";
-        menu.style.top = e.pageY + "px";
-      }
-      table.addEventListener("contextmenu", showMenu);
-      table.addEventListener("click", function() {
-        menu.style.display = "none";
-      });
-      menu.querySelectorAll("li a").forEach(function(item) {
-        item.addEventListener("click", function(e) {
-          e.preventDefault();
-          console.log("Clicked on: " + this.textContent);
-          menu.style.display = "none";
-        });
+        // 선택된 이벤트 확인 console.log("clicked: "+this.textContent);
+        menu.style.display="none";
       });
     });
-    document.addEventListener("click", function() {
-      contextMenus.forEach(function(menu) {
-        menu.style.display = "none";
-      });
+  });
+  document.addEventListener("click", function() {
+    contextMenus.forEach(function(menu) {
+      menu.style.display="none";
     });
+  });
 });
